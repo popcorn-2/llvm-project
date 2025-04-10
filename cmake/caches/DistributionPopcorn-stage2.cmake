10bd83f14638ddd00084137b08083f912d9e2daf
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2025-04-10 18:28:25 +0100

    Use libcxx

--- cmake/caches/DistributionPopcorn-stage2.cmake
+++ cmake/caches/DistributionPopcorn-stage2.cmake
@@ -2,7 +2,8 @@
 # bootstrap build.
 
 set(LLVM_ENABLE_PROJECTS "clang;clang-tools-extra;lld;lldb" CACHE STRING "")
-set(LLVM_ENABLE_RUNTIMES "compiler-rt;libcxx;libcxxabi" CACHE STRING "")
+set(LLVM_ENABLE_RUNTIMES "compiler-rt;libcxx;libcxxabi;libunwind" CACHE STRING "")
+set(LLVM_ENABLE_LIBCXX "ON" CACHE BOOL "")
 
 set(LLVM_TARGETS_TO_BUILD X86 CACHE STRING "")
 
