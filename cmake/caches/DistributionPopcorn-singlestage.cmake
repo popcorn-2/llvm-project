3bfa2f8563bdda47cc9b64c7bea14b6a49fc8b49
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2025-04-14 21:14:47 +0100

    pls pls work

--- cmake/caches/DistributionPopcorn-singlestage.cmake
+++ cmake/caches/DistributionPopcorn-singlestage.cmake
@@ -2,13 +2,54 @@
 
 #Enable LLVM projects and runtimes
 set(LLVM_ENABLE_PROJECTS "clang;clang-tools-extra;lld" CACHE STRING "")
-set(LLVM_ENABLE_RUNTIMES "" CACHE STRING "")
+set(LLVM_ENABLE_RUNTIMES "compiler-rt;libcxx;libcxxabi;libunwind" CACHE STRING "")
 
 set(CLANG_DEFAULT_CXX_STDLIB libc++ CACHE STRING "")
 set(CLANG_DEFAULT_LINKER lld CACHE STRING "")
 set(CLANG_DEFAULT_OBJCOPY llvm-objcopy CACHE STRING "")
 set(CLANG_DEFAULT_RTLIB compiler-rt CACHE STRING "")
 set(CLANG_DEFAULT_UNWINDLIB libunwind CACHE STRING "")
+
+set(LIBUNWIND_ENABLE_SHARED OFF CACHE BOOL "")
+set(LIBUNWIND_INSTALL_LIBRARY ON CACHE BOOL "")
+set(LIBUNWIND_USE_COMPILER_RT ON CACHE BOOL "")
+
+list(APPEND BUILTIN_TARGETS "default")
+list(APPEND BUILTIN_TARGETS "x86_64-unknown-popcorn")
+list(APPEND RUNTIME_TARGETS "default")
+list(APPEND RUNTIME_TARGETS "x86_64-unknown-popcorn")
+set(RUNTIMES_default_LLVM_ENABLE_RUNTIMES "compiler-rt;libcxx;libcxxabi;libunwind" CACHE STRING "")
+set(RUNTIMES_x86_64-unknown-popcorn_LLVM_ENABLE_RUNTIMES "compiler-rt;libunwind" CACHE STRING "")
+
+set(BUILTINS_x86_64-unknown-popcorn_CMAKE_SYSTEM_NAME Generic CACHE STRING "")
+set(BUILTINS_x86_64-unknown-popcorn_CMAKE_C_FLAGS "--target=x86_64-unknown-popcorn --sysroot=${POPCORN_SYSROOT}" CACHE STRING "")
+set(BUILTINS_x86_64-unknown-popcorn_CMAKE_CXX_FLAGS "--target=x86_64-unknown-popcorn --sysroot=${POPCORN_SYSROOT}" CACHE STRING "")
+set(BUILTINS_x86_64-unknown-popcorn_CMAKE_EXE_LINKER_FLAGS "--target=x86_64-unknown-popcorn --sysroot=${POPCORN_SYSROOT}" CACHE STRING "")
+set(BUILTINS_x86_64-unknown-popcorn_CMAKE_SHARED_LINKER_FLAGS "--target=x86_64-unknown-popcorn --sysroot=${POPCORN_SYSROOT}" CACHE STRING "")
+set(BUILTINS_x86_64-unknown-popcorn_CMAKE_MODULE_LINKER_FLAGS "--target=x86_64-unknown-popcorn --sysroot=${POPCORN_SYSROOT}" CACHE STRING "")
+set(BUILTINS_x86_64-unknown-popcorn_SYSROOT "${POPCORN_SYSROOT}")
+set(RUNTIMES_x86_64-unknown-popcorn_CMAKE_SYSTEM_NAME Popcorn CACHE STRING "")
+set(RUNTIMES_x86_64-unknown-popcorn_CMAKE_C_FLAGS "--target=x86_64-unknown-popcorn --sysroot=${POPCORN_SYSROOT}" CACHE STRING "")
+set(RUNTIMES_x86_64-unknown-popcorn_CMAKE_CXX_FLAGS "--target=x86_64-unknown-popcorn --sysroot=${POPCORN_SYSROOT}" CACHE STRING "")
+set(RUNTIMES_x86_64-unknown-popcorn_CMAKE_EXE_LINKER_FLAGS "--target=x86_64-unknown-popcorn --sysroot=${POPCORN_SYSROOT}" CACHE STRING "")
+set(RUNTIMES_x86_64-unknown-popcorn_CMAKE_SHARED_LINKER_FLAGS "--target=x86_64-unknown-popcorn --sysroot=${POPCORN_SYSROOT}" CACHE STRING "")
+set(RUNTIMES_x86_64-unknown-popcorn_CMAKE_MODULE_LINKER_FLAGS "--target=x86_64-unknown-popcorn --sysroot=${POPCORN_SYSROOT}" CACHE STRING "")
+set(RUNTIMES_x86_64-unknown-popcorn_SYSROOT "${POPCORN_SYSROOT}")
+
+set(BUILTINS_CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/../toolchain" CACHE PATH "")
+
+set(RUNTIMES_x86_64-unknown-popcorn_CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/../sysroot_new/system" CACHE PATH "")
+set(RUNTIMES_default_CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/../toolchain" CACHE PATH "")
+
+set(LLVM_BUILTIN_TARGETS "${BUILTIN_TARGETS}" CACHE STRING "")
+set(LLVM_RUNTIME_TARGETS "${RUNTIME_TARGETS}" CACHE STRING "")
+
+set(COMPILER_RT_BUILD_BUILTINS ON CACHE BOOL "")
+set(COMPILER_RT_BUILD_LIBFUZZER OFF CACHE BOOL "")
+set(COMPILER_RT_BUILD_MEMPROF OFF CACHE BOOL "")
+set(COMPILER_RT_BUILD_PROFILE OFF CACHE BOOL "")
+set(COMPILER_RT_BUILD_SANITIZERS OFF CACHE BOOL "")
+set(COMPILER_RT_BUILD_XRAY OFF CACHE BOOL "")
 
 set(LLVM_TARGETS_TO_BUILD "Native;X86" CACHE STRING "")
 
@@ -53,5 +94,7 @@ set(LLVM_DISTRIBUTION_COMPONENTS
   clang-format
   clang-resource-headers
   lld
+  builtins
+  runtimes
   ${LLVM_TOOLCHAIN_TOOLS}
   CACHE STRING "")
